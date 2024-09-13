5a32b496e1d91e1242fca8f6c1b0e209118637c3
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-13 20:42:44 +0200

    input chane event

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import useQuize from "./Store";
 
 export default function Home() {
   const quizeConfige = useQuize(state=> state.config)
+  const addNumberOfQuestions = useQuize(state => state.addNumberOfQuestions)
   console.log(quizeConfige)
   return (
     <section className="flex flex-col justify-center items-center my-10">
@@ -13,7 +14,7 @@ export default function Home() {
       <section className="my-10 rounded-lg p-10 shadow-xl lg:w-[75%] w-[80%]">
         <div>
           <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Questions</label>
-          <input type="number" id="first_name" defaultValue={10} min={0} max={50} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
+          <input type="number" id="first_name" defaultValue={10} min={0} max={50}  onChange={(e) => addNumberOfQuestions(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
         </div>
         <div className="mx-auto flex flex-col justify-center w-full">
         <DropDownOtions/>
